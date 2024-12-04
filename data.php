49f1b59d263d6b92f2722e42633ebe05fbe73476
Muhammad Rizqi Ferhinandito <120573126+mrfdito@users.noreply.github.com>
2024-12-04 11:57:04 +0700

    Update data.php

--- data.php
+++ data.php
@@ -1,25 +1,72 @@
 <?php
-// File: data.php
+// Data Kuis
 $quizData = [
-    [
-        'type' => 'multiple',
-        'question' => 'Apa ibu kota Indonesia?',
-        'options' => ['Jakarta', 'Surabaya', 'Bandung', 'Medan'],
-        'correct' => 'Jakarta',
-        'points' => 5
-    ],
-    [
-        'type' => 'text',
-        'question' => 'Apa nama lagu kebangsaan Indonesia? (jawab dengan huruf besar diawal)',
-        'correct' => 'Indonesia Raya',
-        'points' => 10
-    ],
-    [
-        'type' => 'multiple',
-        'question' => 'Pulau manakah yang terbesar di Indonesia?',
-        'options' => ['Jawa', 'Sumatra', 'Kalimantan', 'Papua'],
-        'correct' => 'Papua',
-        'points' => 5
-    ],
-    // Tambahkan data lainnya sesuai kebutuhan
+    'questions' => [
+        [
+            'type' => 'multiple',
+            'question' => 'Apa ibu kota Indonesia?',
+            'options' => ['Jakarta', 'Surabaya', 'Bandung', 'Medan'],
+            'correct' => 'Jakarta',
+            'points' => 5
+        ],
+        [
+            'type' => 'text',
+            'question' => 'Apa nama lagu kebangsaan Indonesia? (jawab dengan huruf besar diawal)',
+            'correct' => 'Indonesia Raya',
+            'points' => 10
+        ],
+        [
+            'type' => 'multiple',
+            'question' => 'Pulau manakah yang terbesar di Indonesia?',
+            'options' => ['Jawa', 'Sumatra', 'Kalimantan', 'Papua'],
+            'correct' => 'Papua',
+            'points' => 5
+        ],
+        [
+            'type' => 'text',
+            'question' => 'Pada tahun berapa Indonesia mendeklarasikan kemerdekaannya? (tahun)',
+            'correct' => '1945',
+            'points' => 10
+        ],
+        [
+            'type' => 'multiple',
+            'question' => 'Siapa presiden pertama Indonesia?',
+            'options' => ['Soekarno', 'Soeharto', 'Habibie', 'Gus Dur'],
+            'correct' => 'Soekarno',
+            'points' => 5
+        ],
+        [
+            'type' => 'text',
+            'question' => 'Pempek berasal dari? (jawab dengan huruf besar diawal)',
+            'correct' => 'Palembang',
+            'points' => 10
+        ],
+        [
+            'type' => 'multiple',
+            'question' => 'Di kota manakah terdapat Jembatan Ampera?',
+            'options' => ['Palembang', 'Jakarta', 'Surabaya', 'Bandung'],
+            'correct' => 'Palembang',
+            'points' => 5
+        ],
+        [
+            'type' => 'text',
+            'question' => 'Ibu kota Jawa Timur adalah? (jawab dengan huruf besar diawal)',
+            'correct' => 'Surabaya',
+            'points' => 10
+        ],
+        [
+            'type' => 'multiple',
+            'question' => 'Apa nama pulau tempat terletaknya ibu kota Indonesia?',
+            'options' => ['Sumatra', 'Jawa', 'Kalimantan', 'Sulawesi'],
+            'correct' => 'Jawa',
+            'points' => 5
+        ],
+        [
+            'type' => 'text',
+            'question' => 'Berapa lama indonesia dijajah oleh Belanda? (tahun)',
+            'correct' => '350',
+            'points' => 10
+        ]
+    ]
 ];
+?>
